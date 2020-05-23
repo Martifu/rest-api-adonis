daca7c294838ba3c06434867390acb0e736f7053
DaniGadget18 <dani_181298@hotmail.com>
2020-05-23 15:07:20 -0500

    cambios

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,53 +1,53 @@
 'use strict'
 
-const {validate} = use('Validator')
+const { validate } = use('Validator')
 const User = use('App/Models/User')
 
 class UserController {
 
 
-  async login({request, auth ,response}){
-    const {email, password} = request.all();
-    const token = await auth.attempt(email, password);
-    
-    return response.status(200).send({'message':"Iniciado", data:token});
-  }
+    async login({ request, auth, response }) {
+        const { email, password } = request.all();
+        const token = await auth.attempt(email, password);
 
-
-  async signup({request, response}){
-    const {email, password, username, f_name, l_name} = request.all();
-    console.log(email,password);
-    const validation = await validate(request.all(), {
-      email: 'required|email',
-      username: 'required',
-      f_name: 'required',
-      l_name: 'required',
-      password: 'required|min:5'
-    });
-
-    if (validation.fails()){
-      return response.status(400).send({message:validation.messages()})
+        return response.status(200).send({ 'message': "Ok", data: token });
     }
 
-    const userFound = await User.findBy('email', email);
-    if (userFound){
-      return  response.send({
-        message: 'Ya existe un usuario creado con ese email.'
-      });
+
+    async signup({ request, response }) {
+        const { email, password, username, f_name, l_name } = request.all();
+        console.log(email, password);
+        const validation = await validate(request.all(), {
+            email: 'required|email',
+            username: 'required',
+            f_name: 'required',
+            l_name: 'required',
+            password: 'required|min:5'
+        });
+
+        if (validation.fails()) {
+            return response.status(400).send({ message: validation.messages() })
+        }
+
+        const userFound = await User.findBy('email', email);
+        if (userFound) {
+            return response.send({
+                message: 'Ya existe un usuario creado con ese email.'
+            });
+        }
+
+        const user = await User.create({
+            email,
+            password,
+            username,
+            f_name,
+            l_name
+        });
+
+        return this.login(...arguments);
+
+        //return response.status(200).send({message:'Has creado tu usuario con exito.'})
     }
-
-    const user = await User.create({
-      email,
-      password,
-      username,
-      f_name,
-      l_name
-    });
-
-    return this.login(...arguments);
-
-    //return response.status(200).send({message:'Has creado tu usuario con exito.'})
-  }
 }
 
 module.exports = UserController
